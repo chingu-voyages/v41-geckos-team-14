9aabe8b1a3062e868730f337b3bfb64bfa28703f
Daniela Küster <daniela.kuester@gmx.net>
2022-10-21 18:50:18 +0200

    Add: Landing Page Component

---
+++ src/landing.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css';
+import Button from "./button";
+
+function Landing() {
+
+    return(
+        <div className="main">
+            <h1 className="landing-heading">Task Management & To-Do List</h1>
+            <p className="landing-description">This productivity tool is designed to manage all of your taks and projects in one place!</p>
+            <div className="landing-buttons">
+                <Button label="Log In"></Button>
+                <Button label="Sign Up"></Button>
+            </div>
+        </div>
+    )
+
+}
+
+export default Landing;
