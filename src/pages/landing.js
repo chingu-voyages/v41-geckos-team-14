f888237fc3a0eb21ebc29faafc1ab1831830d316
hemahafez01 <hemahafez01@gmail.com>
2022-11-02 23:51:17 +0200

    Squashed commit of the following:
    commit 23523257bf366cdc57009e61fec9c6c91ac7e70f
    Merge: 4e76614 e786f0a
    Author: hemahafez01 <58659817+hemahafez01@users.noreply.github.com>
    Date:   Wed Nov 2 10:17:59 2022 +0200
    
        Merge pull request #19 from chingu-voyages/feature/login-page
    
        Add: Login Component with MVP Design
    
    commit e786f0a542ced41031e5dca0f5941be9a1ad7d1f
    Author: Daniela Küster <daniela.kuester@gmx.net>
    Date:   Tue Nov 1 11:46:00 2022 +0100
    
        Add: Login Component with MVP Design

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import '../App.css';
 import Button from "../component/button";
+import './App.css';
+import Button from "./button";
+import productive from "./img/landing-page-mobile.png";
 
 function Landing() {
 
     return(
-        <div className="main">
+        <div className="main landing-page">
             <h1 className="landing-heading">Task Management & To-Do List</h1>
             <p className="landing-description">This productivity tool is designed to manage all of your taks and projects in one place!</p>
+            <img className="productive-woman" src={productive} alt="Woman with task items"/>
             <div className="landing-buttons">
                 <Link to="/login" ClassName=""><Button label="Log In"></Button></Link>
                 <Link to="/signup" ClassName=""><Button label="Sign Up"></Button></Link>
