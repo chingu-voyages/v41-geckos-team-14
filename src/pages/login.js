8d21618b610efc4bc89eade51ecbf71e9df0d311
Daniela Küster <daniela.kuester@gmx.net>
2022-11-11 18:58:08 +0100

    Add: Make API post call - still a WIP

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,39 +5,46 @@ import Button from "../component/button";
 function Login() {
 
     // Define variables in state
-    const [userName, setUserName] = useState("");
+    const [username, setUserName] = useState("");
     const [password, setPassword] = useState("");
-    const [postId, setPostId] = useState(null);
-
-    // Make API Post call with React hook useEffect (code modified from https://jasonwatmore.com/post/2020/02/01/react-fetch-http-post-request-examples)
-    useEffect(() => {
-        // POST request using fetch inside useEffect React hook
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ title: 'React Hooks POST Request Example' })
-        };
-        fetch('https://reqres.in/api/posts', requestOptions)
-            .then(response => response.json())
-            .then(data => setPostId(data.id));
-
-    // empty dependency array means this effect will only run once (like componentDidMount in classes)
-    }, []);
+    const [message, setMessage] = useState("");
 
     //Add functions to handle the data from the input fields
-
-    const onSubmit = (event) => {
-            event.preventDefault();
-            console.log("I am submitting: " + userName);
-            console.log("I am submitting: " + password);
-        }
-    
     const handleUsernameChange = (event) => {
         setUserName(event.target.value);
     }
 
     const handlePasswordChange = (event) => {
         setPassword(event.target.value);
+    }
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const loginData = {username, password}
+        console.log("User name is: " + username);
+        console.log(typeof username);
+        console.log("Password is: " + password);
+        console.log(typeof password);
+
+        try {
+            let res = await fetch("https://todoapi.fly.dev/api/login", {
+                method: "POST",
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                }),
+            });
+            if (res.status === 200) {
+                console.log(res.status);
+                setMessage("User logged in successfully");
+            } else if (res.status === 422) {
+                setMessage("Invalid credentials.");
+            } else {
+                setMessage("Some error occured");
+            }
+          } catch (error) {
+                console.log(error);
+          }
     }
 
     return(
@@ -47,15 +54,14 @@ function Login() {
             <form onSubmit={onSubmit}>
                 <div className="login-group">
                     <label>
-                        User Name: <input className="input" name="username" type="text" value={userName} onChange={handleUsernameChange}/>
+                        User Name: <input className="input" name="username" type="text" value={username} onChange={handleUsernameChange}/>
                     </label>
                     <label>
                         Password: <input className="input" name="password" type="text" value={password} onChange={handlePasswordChange}/>
                     </label>
                     <Button className="button login-button" label="Log In" type="submit" ></Button>
                     <div>
-                        <p>You say: {userName}, {password}</p>
-                        <p>Returned Id: {postId}</p>
+                        <p>Login status: {message}</p>
                     </div>
                 </div>
             </form>
